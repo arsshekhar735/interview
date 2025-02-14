d5c916410475b33902d59f4273007e65eaafa2ca
Suman Shekhar <suman.nwd03@gmail.com>
2025-02-14 10:57:05 +0530

    updated

---
+++ revision.js
@@ -0,0 +1,7 @@
+// let a = 25;
+// a = "radhe";
+// console.log(a)//this happens because dynamic typed language
+
+// const b = 25
+// b = 20
+// console.log(b)=> This can't be redeclared, reassign and can't change the value
